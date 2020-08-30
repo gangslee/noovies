f4914d4afd8fda2ace43b9610c09a79882fe8428
gangslee <grandnet1225@gmail.com>
2020-08-30 20:15:49 +0900

    Set cacheImages

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import {AppLoading} from 'expo';
+import {Asset} from 'expo-asset';
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = async () => {};
+  const loadAssets = async () => {
+    const images = cacheImages([
+      'https://images.unsplash.com/photo-1584486188544-dc2e1417aff1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      require('./assets/splash.png'),
+    ]);
+  };
   const onFinish = () => setIsReady(true);
 
   return isReady ? null : (
