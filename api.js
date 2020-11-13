1589fb18772f4d27fd7ad9743408d6eed404d346
이경수 <grandnet@naver.com>
2020-11-13 17:40:18 +0900

    Detail style 1

--- api.js
+++ api.js
@@ -40,7 +40,7 @@ export const tvApi = {
   show: (id) => getAnything(`/tv/${id}`),
 };
 
-export const apiImage = (path) =>
-  path
-    ? `https://image.tmdb.org/t/p/w500${path}`
-    : "https://images.unsplash.com/photo-1497514440240-3b870f7341f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+export const apiImage = (
+  path,
+  defaultPoster = "https://images.unsplash.com/photo-1497514440240-3b870f7341f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+) => (path ? `https://image.tmdb.org/t/p/w500${path}` : defaultPoster);
