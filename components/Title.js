04cb55133c8d42f66864f8bff593a6e7b5952732
이경수 <grandnet@naver.com>
2020-11-13 18:01:33 +0900

    Detail styles 2

--- components/Title.js
+++ components/Title.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components/native';
-import PropTypes from 'prop-types';
+import React from "react";
+import styled from "styled-components/native";
+import PropTypes from "prop-types";
 
 const Text = styled.Text`
   color: white;
--- screens/Detail.js
+++ screens/Detail.js
@@ -1,45 +1,89 @@
 import React from "react";
-import { View, Text, Dimensions } from "react-native";
+import { Dimensions } from "react-native";
 import styled from "styled-components/native";
 import { apiImage } from "../api";
 import Poster from "../components/Poster";
 import ScrollContainer from "../components/ScrollContainer";
 import Votes from "../components/Votes";
 
-const Header = styled.View``;
+const Header = styled.View`
+  height: ${Dimensions.get("window").height / 3}px;
+  align-items: center;
+  justify-content: flex-end;
+`;
 
-const Container = styled.View``;
+const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  top: 40px;
+`;
 
 const BG = styled.Image`
   width: 100%;
-  height: ${Dimensions.get("window").height / 3}px;
+  height: 100%;
   opacity: 0.4;
   position: absolute;
 `;
 
-const Title = styled.Text``;
+const Title = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
 
-const Info = styled.View``;
+const Info = styled.View`
+  width: 50%;
+  margin-left: 50px;
+`;
+
+const Data = styled.View`
+  padding: 0px 30px;
+  margin-top: 80px;
+`;
+
+const DataName = styled.Text`
+  color: white;
+  opacity: 0.8;
+  font-weight: 800;
+  margin-bottom: 5px;
+`;
+
+const DataValue = styled.Text`
+  color: white;
+  opacity: 0.8;
+  font-weight: 500;
+`;
 
 export default ({
   navigation,
   route: {
-    params: { id, title, backgroundImage, poster, votes },
+    params: { id, title, backgroundImage, poster, votes, overview },
   },
 }) => {
   navigation.setOptions({ title });
   return (
     <ScrollContainer loading={false}>
-      <Header>
-        <BG source={{ uri: apiImage(backgroundImage, "-") }} />
-        <Container>
-          <Poster url={poster} />
-          <Info>
-            <Title>{title}</Title>
-            <Votes votes={votes} />
-          </Info>
-        </Container>
-      </Header>
+      <>
+        <Header>
+          <BG source={{ uri: apiImage(backgroundImage, "-") }} />
+          <Container>
+            <Poster url={poster} />
+            <Info>
+              <Title>{title}</Title>
+              {votes && <Votes votes={votes} />}
+            </Info>
+          </Container>
+        </Header>
+        <Data>
+          {overview && (
+            <>
+              <DataName>Overview</DataName>
+              <DataValue>{overview}</DataValue>
+            </>
+          )}
+        </Data>
+      </>
     </ScrollContainer>
   );
 };
