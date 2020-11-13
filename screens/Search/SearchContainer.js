7e679f7daa184a97e4ab22d0810778be556502fd
이경수 <grandnet@naver.com>
2020-11-13 15:41:32 +0900

    Searching Logic

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,4 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { movieApi, tvApi } from "../../api";
 import SearchPresenter from "./SearchPresenter";
 
-export default () => <SearchPresenter />;
+export default () => {
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState({
+    movies: [],
+    shows: [],
+    movieError: null,
+    showsError: null,
+  });
+  const onChange = (text) => {
+    setKeyword(text);
+  };
+  const search = async () => {
+    const [movies, movieError] = await movieApi.search(keyword);
+    const [shows, showsError] = await tvApi.search(keyword);
+    setResults({
+      movies,
+      shows,
+      movieError,
+      showsError,
+    });
+  };
+  console.log(results);
+  return (
+    <SearchPresenter
+      {...results}
+      onChange={onChange}
+      onSubmit={search}
+      keyword={keyword}
+    />
+  );
+};
--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,13 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
+import Horizontal from "../../components/Horizontal";
+import HorizontalSlider from "../../components/HorizontalSlider";
 import Input from "../../components/Search/Input";
 
 const Container = styled.ScrollView`
   background-color: black;
 `;
 
-export default () => (
+export default ({ keyword, onSubmit, onChange }) => (
   <Container>
-    <Input placeholder="Write a keyword" />
+    <Input
+      placeholder="Write a keyword"
+      value={keyword}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+    <HorizontalSlider title="Movie Results">
+      {movies.map((movie) => (
+        <Horizontal />
+      ))}
+    </HorizontalSlider>
+    <HorizontalSlider title="TV Results">
+      {shows.map((show) => (
+        <Horizontal />
+      ))}
+    </HorizontalSlider>
   </Container>
 );
